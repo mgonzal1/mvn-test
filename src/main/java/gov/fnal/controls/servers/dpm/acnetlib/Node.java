19ee4d3328d96f5932d3fd29ea08ffe72088a727
ukandula <156366497+ukandula@users.noreply.github.com>
2024-04-26 10:27:51 -0500

    latest changes

--- src/main/java/gov/fnal/controls/servers/dpm/acnetlib/Node.java
+++ src/main/java/gov/fnal/controls/servers/dpm/acnetlib/Node.java
@@ -1,221 +1,197 @@
 // $Id: Node.java,v 1.3 2024/03/19 22:13:05 kingc Exp $
 package gov.fnal.controls.servers.dpm.acnetlib;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.BitSet;
-import java.util.HashMap;
-import java.util.logging.Level;
-import java.net.InetAddress;
 import java.net.Inet4Address;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.sql.ResultSet;
+import java.util.*;
+import java.util.logging.Level;
 
 import static gov.fnal.controls.db.DbServer.getDbServer;
 
-public class Node implements NodeFlags, AcnetErrors
-{
-	final static HashMap<Integer, Node> byValue = new HashMap<>();
-	final static HashMap<String, Node> byName = new HashMap<>();
+public class Node implements NodeFlags, AcnetErrors {
+    final static HashMap<Integer, Node> byValue = new HashMap<>();
+    final static HashMap<String, Node> byName = new HashMap<>();
 
-	final int value;
-	final String name;
-	final InetSocketAddress address; 
-	final BitSet flags;
+    static {
+        System.setProperty("java.util.logging.SimpleFormatter.format", "[%4$-8s %1$tF %1$tT %1$tL] %5$s %6$s%n");
+    }
 
-	static {
-		System.setProperty("java.util.logging.SimpleFormatter.format", "[%4$-8s %1$tF %1$tT %1$tL] %5$s %6$s%n");
-	}
+    final int value;
+    final String name;
+    final InetSocketAddress address;
+    final BitSet flags;
 
-	Node()
-	{
-		this.value = 0;
-		this.name = "";
-		this.address = null;
-		this.flags = null;
-	}
-	
-	private Node(int value, String name, String host) 
-	{
-		this.value = value;
-		this.name = name;
-		this.address = new InetSocketAddress(host, 6801);
-		this.flags = new BitSet();
-	}
+    Node() {
+        this.value = 0;
+        this.name = "";
+        this.address = null;
+        this.flags = null;
+    }
 
-	private Node(ResultSet rs) throws AcnetStatusException
-	{
-		try {
-			this.value = (rs.getInt("trunk") << 8) + rs.getInt("node");
-			this.name = rs.getString("node_name").trim();
-			this.address = new InetSocketAddress(rs.getString("ip_name").trim(), 6801);
-			
-			final String status = rs.getString("status").trim();
+    private Node(int value, String name, String host) {
+        this.value = value;
+        this.name = name;
+        this.address = new InetSocketAddress(host, 6801);
+        this.flags = new BitSet();
+    }
 
-			this.flags = new BitSet();
-			this.flags.set(HARDWARE_CLOCK, rs.getBoolean("is_tclk_fe"));
-			this.flags.set(EVENT_STRING_SUPPORT, rs.getBoolean("is_gets32_sets32"));
-			this.flags.set(OPERATIONAL, status.equals("Operational"));
-			this.flags.set(OBSOLETE, status.equals("Obsolete"));
-			this.flags.set(OUT_OF_SERVICE, status.startsWith("Out of Service"));
-		} catch (Exception e) {
-			throw new AcnetStatusException(SQL_USER0, "Node", e);
-		}
-	}
+    private Node(ResultSet rs) throws AcnetStatusException {
+        try {
+            this.value = (rs.getInt("trunk") << 8) + rs.getInt("node");
+            this.name = rs.getString("node_name").trim();
+            this.address = new InetSocketAddress(rs.getString("ip_name").trim(), 6801);
 
-	public synchronized static List<Node> get(InetAddress addr)
-	{
-		final ArrayList<Node> nodes = new ArrayList<>();
+            final String status = rs.getString("status").trim();
 
-		for (Node node : byName.values()) {
-			final InetAddress tmp = node.address.getAddress();
-			if (tmp != null && addr.equals(tmp))
-				nodes.add(node);
-		}
+            this.flags = new BitSet();
+            this.flags.set(HARDWARE_CLOCK, rs.getBoolean("is_tclk_fe"));
+            this.flags.set(EVENT_STRING_SUPPORT, rs.getBoolean("is_gets32_sets32"));
+            this.flags.set(OPERATIONAL, status.equals("Operational"));
+            this.flags.set(OBSOLETE, status.equals("Obsolete"));
+            this.flags.set(OUT_OF_SERVICE, status.startsWith("Out of Service"));
+        } catch (Exception e) {
+            throw new AcnetStatusException(SQL_USER0, "Node", e);
+        }
+    }
 
-		return nodes;
-	}
+    public synchronized static List<Node> get(InetAddress addr) {
+        final ArrayList<Node> nodes = new ArrayList<>();
 
-	public synchronized static Node get(int value) throws AcnetStatusException
-	{
-		final Node node = byValue.get(value);
+        for (Node node : byName.values()) {
+            final InetAddress tmp = node.address.getAddress();
+            if (addr.equals(tmp))
+                nodes.add(node);
+        }
 
-		if (node != null)
-			return node;
+        return nodes;
+    }
 
-		throw new AcnetStatusException(ACNET_NO_NODE);
-	}
+    public synchronized static Node get(int value) throws AcnetStatusException {
+        final Node node = byValue.get(value);
 
-	public synchronized static Node get(String name) throws AcnetStatusException
-	{
-		final Node node = byName.get(name.trim().toUpperCase());
+        if (node != null)
+            return node;
 
-		if (node != null)
-			return node;
+        throw new AcnetStatusException(ACNET_NO_NODE);
+    }
 
-		throw new AcnetStatusException(ACNET_NO_NODE);
-	}
+    public synchronized static Node get(String name) throws AcnetStatusException {
+        final Node node = byName.get(name.trim().toUpperCase());
 
-	synchronized public static void init()
-	{
-		try {
-			cacheNodes();
-		} catch (Exception e) {
-			throw new RuntimeException("Exception caching nodes" , e);
-		}
-	}
+        if (node != null)
+            return node;
 
-	final public InetSocketAddress address()
-	{
-		return address;
-	}
+        throw new AcnetStatusException(ACNET_NO_NODE);
+    }
 
-	final public int value()
-	{
-		return value;
-	}
+    synchronized public static void init() {
+        try {
+            cacheNodes();
+        } catch (Exception e) {
+            throw new RuntimeException("Exception caching nodes", e);
+        }
+    }
 
-	public String name()
-	{
-		return name;
-	}
+    public synchronized final static void cacheNodes() throws AcnetStatusException {
+        AcnetInterface.logger.log(Level.INFO, "Caching nodes");
 
-	final public boolean isValid()
-	{
-		if (address != null) {
-			final InetAddress addr = address.getAddress();
+        final String query = "SELECT N.trunk, N.node, I.node_name, N.ip_name, I.status, I.is_tclk_fe, I.is_gets32_sets32 FROM " +
+                "accdb.acnet_node N, johnson.acnet_view_node_info I WHERE I.node_name = N.node_name ";
 
-			return addr instanceof Inet4Address && addr.getAddress() != null;
-		}
+        byValue.clear();
+        byName.clear();
 
-		return false;
-	}
+        ResultSet rs = null;
 
-	public boolean is(int flagNo)
-	{
-		return flags.get(flagNo);
-	}
+        try {
+            rs = getDbServer("adbs").executeQuery(query);
 
-	public boolean has(int flagNo)
-	{
-		return flags.get(flagNo);
-	}
+            while (rs.next()) {
+                final Node node = new Node(rs);
 
-	public synchronized final static void cacheNodes() throws AcnetStatusException
-	{
-		AcnetInterface.logger.log(Level.INFO, "Caching nodes");
+                byValue.put(node.value, node);
+                byName.put(node.name, node);
+            }
+        } catch (Exception e) {
+            throw new AcnetStatusException(SQL_USER0, "getNodes " + e, e);
+        } finally {
+            try {
+                if (rs != null)
+                    rs.close();
+            } catch (Exception e) {
+            }
+        }
 
-		final String query = "SELECT N.trunk, N.node, I.node_name, N.ip_name, I.status, I.is_tclk_fe, I.is_gets32_sets32 FROM " +
-								"accdb.acnet_node N, johnson.acnet_view_node_info I WHERE I.node_name = N.node_name ";
+        final Node mcast = new Node(255, "MCAST", "239.128.4.1");
 
-		byValue.clear();
-		byName.clear();
+        byValue.put(mcast.value(), mcast);
+        byName.put(mcast.name(), mcast);
+    }
 
-		ResultSet rs = null;
+    public static void main(String[] args) throws AcnetStatusException {
+        cacheNodes();
 
-		try {
-			rs = getDbServer("adbs").executeQuery(query);
+        for (java.util.Map.Entry<String, Node> entry : (new java.util.TreeMap<String, Node>(byName)).entrySet()) {
+            final Node node = entry.getValue();
+            System.out.printf("value:0x%04x name:%-10s oper:%-6s clock:%-6s %8s ip_name:%s\n",
+                    node.value, node.name, node.is(OPERATIONAL),
+                    node.has(HARDWARE_CLOCK), node.has(EVENT_STRING_SUPPORT) ? "GETS32" : "RETDAT", node.address);
+        }
 
-			while (rs.next()) {
-				final Node node = new Node(rs);
+        System.exit(0);
+    }
 
-				byValue.put(node.value, node);
-				byName.put(node.name, node);
-			}
-		} catch (Exception e) {
-			throw new AcnetStatusException(SQL_USER0, "getNodes " + e, e);
-		} finally {
-			try {
-				if (rs != null)
-					rs.close();
-			} catch (Exception e) {
-			}
-		}
+    final public InetSocketAddress address() {
+        return address;
+    }
 
-		final Node mcast = new Node(255, "MCAST", "239.128.4.1");
+    final public int value() {
+        return value;
+    }
 
-		byValue.put(mcast.value(), mcast);
-		byName.put(mcast.name(), mcast);
-	}
+    public String name() {
+        return name;
+    }
 
-	public boolean isMCAST()
-	{
-		return value == 255;
-	}
+    final public boolean isValid() {
+        if (address != null) {
+            final InetAddress addr = address.getAddress();
 
-	@Override
-	public boolean equals(Object o)
-	{
-		if (o instanceof Node)
-			return ((Node) o).value == value;
+            return addr instanceof Inet4Address && addr.getAddress() != null;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	@Override
-	public int hashCode()
-	{
-		return Objects.hash(value);
-	}
+    public boolean is(int flagNo) {
+        return flags.get(flagNo);
+    }
 
-	@Override
-	public String toString()
-	{
-		return name;
-	}
+    public boolean has(int flagNo) {
+        return flags.get(flagNo);
+    }
 
-	public static void main(String[] args) throws AcnetStatusException
-	{
-		cacheNodes();
+    public boolean isMCAST() {
+        return value == 255;
+    }
 
-		for (java.util.Map.Entry<String, Node> entry : (new java.util.TreeMap<String, Node>(byName)).entrySet()) {
-			final Node node = entry.getValue();
-			System.out.printf("value:0x%04x name:%-10s oper:%-6s clock:%-6s %8s ip_name:%s\n", 
-									node.value, node.name, node.is(OPERATIONAL), 
-									node.has(HARDWARE_CLOCK), node.has(EVENT_STRING_SUPPORT) ? "GETS32" : "RETDAT", node.address);
-		}
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Node)
+            return ((Node) o).value == value;
 
-		System.exit(0);
-	}
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
