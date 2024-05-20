17a666c0126f3a64c1cea836bd71e80796b0e798
ukandula <156366497+ukandula@users.noreply.github.com>
2024-05-20 15:30:34 -0500

    Renamed file name as we have ClockEvent in two modules. So, re pushing the test cases again to test.

---
+++ src/test/java/gov/fnal/controls/servers/dpm/events/EventsClockEventTest.java
@@ -0,0 +1,41 @@
+package gov.fnal.controls.servers.dpm.events;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class EventsClockEventTest {
+
+    private static final int MAX_NUMBER_EVENTS = 256;
+    private static final String[] names = new String[MAX_NUMBER_EVENTS];
+
+    @Test
+    public void ClockEventTest1() {
+        ClockEvent clockEvent = new ClockEvent(8900, false, 2000L, true);
+
+    }
+    @Test
+    public void ClockEventTest2() {
+        ClockEvent clockEvent = new ClockEvent(8900, true, 2000L, true);
+
+    }
+    @Test
+    public void ClockEventTest3() {
+        ClockEvent clockEvent = new ClockEvent(8900, false, 2000L, false);
+
+    }
+    @Test
+    public void ClockEventTest4() {
+        ClockEvent clockEvent = new ClockEvent(8900, true, 2000L, false);
+
+    }
+
+    @Test
+    public void ClockTest_ftd(){
+
+        ClockEvent clockEvent = new ClockEvent(2000, false, 0L);
+        assertEquals(34768,clockEvent.ftd());
+    }
+
+}
