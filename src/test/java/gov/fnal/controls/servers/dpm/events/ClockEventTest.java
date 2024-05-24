c8d3829bfcc4ff97491ff23e1d8eb48fa773e394
ukandula <156366497+ukandula@users.noreply.github.com>
2024-05-24 11:14:26 -0500

    testing

--- src/test/java/gov/fnal/controls/servers/dpm/events/ClockEventTest.java
+++ src/test/java/gov/fnal/controls/servers/dpm/events/ClockEventTest.java
@@ -12,27 +12,23 @@ public class ClockEventTest {
 
     @Test
     public void ClockTest_ftdWhenDelayIsNotZero(){
-
         ClockEvent clockEvent = new ClockEvent(2000, false, 10);
         assertEquals(0,clockEvent.ftd());
     }
 
     @Test
     public void ClockTest_ftdWhenDelayIsZero(){
-
         ClockEvent clockEvent = new ClockEvent(2000, false);
         assertEquals(34768,clockEvent.ftd());
     }
 
     @Test
     public void ClockTest_ftd(){
-
         ClockEvent clockEvent = new ClockEvent(23237);
         assertEquals(56005,clockEvent.ftd());
     }
     @Test
     public void ClockTest_ClockEventNumberToNameWhenClockEventIsGreaterThan256(){
-
         ClockEvent clockEvent = new ClockEvent(2000);
         assertEquals("invalid clock event", ClockEvent.clockEventNumberToName(350));
    }
